e06cc2ce916ccc70d7c7ed558917b379f23dd21c
Jbtzcodes <laminutedecode@gmail.com>
2024-02-06 16:37:09 +0100

    mise ajour

--- Menu-popup/app.js
+++
@@ -1,6 +0,0 @@
-const container = document.querySelector('.menu-container')
-const btn = document.querySelector('.burger')
-
-btn.addEventListener('click', () => {
-  container.classList.toggle('active')
-}) 
--- Texte-waves-js/app.js
+++
@@ -1,6 +0,0 @@
-const text = document.querySelectorAll('p')
-
-
-text.forEach(txt => {
-    txt.innerHTML = txt.textContent.split('').map((txt, wave) => `<span style="transition-delay: ${wave * 25}ms">${txt}</span>`).join('')
-})
